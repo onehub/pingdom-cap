5082ffc86986dbec8f84922f72202a4f8f31686f
John G. Norman <john@7fff.com>
2012-02-08 14:45:59 +0100

    Rearrange dry_run logging, and green

--- lib/pingdom_cap/capistrano.rb
+++ lib/pingdom_cap/capistrano.rb
@@ -11,16 +11,17 @@ module PingdomCap
             ENV[name.to_s.upcase] || fetch(name)
           end
           def pingdom(configuration, operation)
+            check_name = param(:pingdom_check_name)
+            username   = param(:pingdom_username)
+            password   = param(:pingdom_password)
+            key        = param(:pingdom_key)
             if configuration.dry_run
               logger.info "DRY RUN: pingdom_cap #{check_name} #{operation} not actually run."
             else
-              check_name = param(:pingdom_check_name)
-              username   = param(:pingdom_username)
-              password   = param(:pingdom_password)
-              key        = param(:pingdom_key)
               client = PingdomCap::Client.new(username: username, password: password, key: key)
               client.send(operation, check_name)
             end
+            logger.info("Pingdom: #{operation}d")
           end
 
           desc "Pause checks at Pingdom"
