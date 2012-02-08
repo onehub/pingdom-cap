178e7d75eb149c079e8662911d40113272bd70e8
John G. Norman <john@7fff.com>
2012-02-08 13:47:32 +0100

    Remove unused method, clean up

--- lib/pingdom_cap/capistrano.rb
+++ lib/pingdom_cap/capistrano.rb
@@ -3,25 +3,22 @@ require 'capistrano'
 module PingdomCap
   module Capistrano
 
-    def self.pingdom(configuration, operation)
-   end
-
     def self.load_into(configuration)
       configuration.load do
         namespace :pingdom do
 
-          def self.param(name)
+          def param(name)
             ENV[name.to_s.upcase] || fetch(name)
           end
           def pingdom(configuration, operation)
             if configuration.dry_run
               logger.info "DRY RUN: pingdom_cap #{check_name} #{operation} not actually run."
             else
-               check_name = param(:pingdom_check_name)
-               username   = param(:pingdom_username)
-               password   = param(:pingdom_password)
-               key        = param(:pingdom_key)
-               client = PingdomCap::Client.new(username: username, password: password, key: key)
+              check_name = param(:pingdom_check_name)
+              username   = param(:pingdom_username)
+              password   = param(:pingdom_password)
+              key        = param(:pingdom_key)
+              client = PingdomCap::Client.new(username: username, password: password, key: key)
               client.send(operation, check_name)
             end
           end
