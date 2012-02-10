b1be5a578d057ac92064c93c999191f9946f70b8
John G. Norman <john@7fff.com>
2012-02-10 10:33:34 +0100

    Parameterized steps - WIP

---
+++ features/step_definitions/parameterized_aruba_steps.rb
@@ -0,0 +1,14 @@
+require 'erb'
+
+When /\AERB (.*)\z/ do |*matches|
+  s = ERB.new(matches[0]).result(binding)
+  if matches.size > 1
+    s = <<MULTI
+#{s}
+"""
+#{ERB.new(matches[1]).result(binding)}
+"""
+MULTI
+  end
+  steps "And #{s}"
+end
