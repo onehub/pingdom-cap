ffe468bf4c1e73cc9439d0a6fa4376fdcd289193
John G. Norman <john@7fff.com>
2012-02-10 14:15:16 +0100

    Do it without using steps method

--- features/step_definitions/parameterized_aruba_steps.rb
+++ features/step_definitions/parameterized_aruba_steps.rb
@@ -1,14 +1,6 @@
 require 'erb'
 
 When /\A\(erb\) (.*)\z/ do |*matches|
-  s = ERB.new(matches[0]).result(binding)
-  if matches.size > 1
-    s = <<-MULTI
-    #{s}
-    """
-    #{ERB.new(matches[1]).result(binding)}
-    """
-MULTI
-  end
-  steps "And #{s}"
+  erbes = matches.map { |match| ERB.new(match).result(binding) }
+  step *erbes
 end
