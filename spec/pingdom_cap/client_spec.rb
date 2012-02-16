f889c060a115f059d8e081c724e6d4c0b087ba2e
Brandon Caplan <bcaplan@onehub.com>
2012-02-15 17:13:03 -0800

    Move tests to old hash format

--- spec/pingdom_cap/client_spec.rb
+++ spec/pingdom_cap/client_spec.rb
@@ -3,34 +3,34 @@ require 'spec_helper'
 describe PingdomCap::Client, '.new' do
   subject { PingdomCap::Client }
   it "should raise an error unless a key is specified" do
-    lambda { subject.new(key: nil) }.should raise_error
+    lambda { subject.new(:key => nil) }.should raise_error
   end
   it "should raise an error unless a url is specified" do
-    lambda { subject.new(url: nil) }.should raise_error
+    lambda { subject.new(:url => nil) }.should raise_error
   end
   it "should raise an error unless a username is specified" do
-    lambda { subject.new(username: nil) }.should raise_error
+    lambda { subject.new(:username => nil) }.should raise_error
   end
   it "should raise an error unless a password is specified" do
-    lambda { subject.new(password: nil) }.should raise_error
+    lambda { subject.new(:password => nil) }.should raise_error
   end
   it "should not raise an error when key, url, username, and password are specified" do
-    lambda { subject.new(key: 'some_key', url: 'https://api.pingdom.com', username: 'john@example.com', password: '123456') }.should_not raise_error
+    lambda { subject.new(:key => 'some_key', :url => 'https://api.pingdom.com', :username => 'john@example.com', :password => '123456') }.should_not raise_error
   end
   it "should not raise an error when non-default options are missing" do
-    lambda { subject.new(key: 'some_key', username: 'john@example.com', password: '123456') }.should_not raise_error
+    lambda { subject.new(:key => 'some_key', :username => 'john@example.com', :password => '123456') }.should_not raise_error
   end
   it "should create a connection during initialization" do
-    client = subject.new(key: 'some_key', username: 'john@example.com', password: '123456')
+    client = subject.new(:key => 'some_key', :username => 'john@example.com', :password => '123456')
     client.connection.should_not be_nil
   end
 end
 
 describe PingdomCap::Client do
   subject { PingdomCap::Client.new(
-    username: ENV['PINGDOM_USERNAME'] || 'john@example.com',
-    password: ENV['PINGDOM_PASSWORD'] || 'zzzzyyyyxxxxwwww',
-    key: ENV['PINGDOM_KEY'] || 'aaaabbbbccccddddeeeefffffaaaabbbb'
+    :username => ENV['PINGDOM_USERNAME'] || 'john@example.com',
+    :password => ENV['PINGDOM_PASSWORD'] || 'zzzzyyyyxxxxwwww',
+    :key => ENV['PINGDOM_KEY'] || 'aaaabbbbccccddddeeeefffffaaaabbbb'
   ) }
   let(:check_name) { ENV['PINGDOM_CHECK_NAME'] || '7fff' }
   let(:any_status) { /\"status\" => \"(?:unknown|up|paused)\"/m }
